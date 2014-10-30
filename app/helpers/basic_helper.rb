0a7166e1adfc258d9a8302680b41dfe103eaee25
Marcin Szmacinski <ziptofaf1@gmail.com>
2014-10-30 12:15:36 +0100

    added more probes to see what uses most of the cpu and to show available free space on hdd

--- app/helpers/basic_helper.rb
+++ app/helpers/basic_helper.rb
@@ -2,6 +2,8 @@ module BasicHelper
   require 'mac-address'
   require 'sysinfo'
   require 'open3' #the module to have a sanitized input/output from system calls so we won't have HDD formatted or something similar by mistake
+  require 'usagewatch'
+  require 'sys/filesystem'
   def getMacs
     MacAddress.address.list
   end
@@ -21,15 +23,19 @@ module BasicHelper
     formatted[:minutes] = formatted[:minutes].to_i
     return formatted
   end
-
-  def systemLoad
+  
+  def cores
     core_count = Open3.capture2("nproc")
     core_count = core_count[0].to_i
+    core_count
+  end
+
+  def systemLoad
     avg_load = Open3.capture2("uptime")
     index = avg_load[0].index('load average:')
     avg_load = avg_load[0].slice(index+14, 4)
     avg_load[","]="."
-    return avg_load.to_f/core_count #avg_load can easily exceed 1.00 and should be divided by core count if you want something that makes sense
+    return avg_load.to_f/cores #avg_load can easily exceed 1.00 and should be divided by core count if you want something that makes sense
   end
   private
 
@@ -38,4 +44,13 @@ module BasicHelper
     return sysinfo.uptime
   end
 
+  def freeSpace
+    stat = Sys::Filesystem.stat("/")
+    mb_available = stat.block_size * stat.blocks_available / 1024 / 1024
+    mb_available
+  end
+  def topProcesses
+    usw = Usagewatch
+    usw.uw_cputop
+  end
 end
